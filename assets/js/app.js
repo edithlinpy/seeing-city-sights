a42c08518b484beaff7f50cbfdbc53891be3b309
SajSultan <ksa.sultan1979@yahoo.com>
2023-02-14 23:17:43 +0000

    commit after ediths push

---
+++ assets/js/app.js
@@ -0,0 +1,16 @@
+// // create cards
+// const main = document.querySelector(".main");
+
+// const card = document.createElement("div");
+// // card.classList = "card";
+
+// const resultsCard = `
+//     <img src="./images/London-bridge1.png" alt="" />
+//     <div class="info">
+//     <h3 class="place-name"></h3>
+//     <p class="place-info" id="output"></p>
+//     <span><a href="index.html">Explore</a></span>
+//     </div>
+// `;
+// card.appendChild(resultsCard);
+
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,8 @@
-// create cards
-const main = document.querySelector(".main");
-console.log(main);
-const card = document.createElement("div");
-card.classList = "card";
 
-const resultsCard = `
-    <img src="./images/London-bridge1.png" alt="" />
-    <div class="info">
-    <h3 class="place-name"></h3>
-    <p class="place-info" id="output"></p>
-    <span><a href="index.html">Explore</a></span>
-    </div>
-`;
 
+
+
+// Start of api search functions
 let requestOptions = {
   method: "GET",
 };
@@ -120,6 +110,7 @@ function getCitySights(cityName) {
             cityObj.name = element.properties.name;
             cityObj.address = element.properties.address_line2;
             sightsData.push(cityObj);
+            
           }
         });
       })
