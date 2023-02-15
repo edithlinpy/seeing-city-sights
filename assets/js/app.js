61ad9a795d4acf921ee05177cc50861a586fa0a0
SajSultan <ksa.sultan1979@yahoo.com>
2023-02-15 14:58:11 +0000

    commit to pull fresh

--- assets/js/app.js
+++ assets/js/app.js
@@ -13,4 +13,17 @@
 //     </div>
 // `;
 // card.appendChild(resultsCard);
-
+function searchPhotos() {
+    let clientId = "dWC_QC6wp6Nk4NUsbXyKxr-eG4QH3PrBf100kAiIh6k";
+    let query = document.getElementById("search").value;
+    let url = "https://api.unsplash.com/search/photos/?client_id="+clientId+"&query="+query;
+  // make fetch request to the unsplash.api
+    fetch(url)
+    .then(function (data) {
+      return data.json()
+    })
+    .then(function (data) {
+      console.log(data)
+  
+    })
+  }
